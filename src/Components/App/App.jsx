36e33fd9e671c73589c9fa59794a483395b59315
Davey-M <davey.meuer@gmail.com>
2022-05-04 11:27:18 -0500

    wip home page

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,7 +1,32 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 
+// component imports
+import Home from '../Home/Home';
+
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <>
+        <header>
+          <nav>
+            <Link to='/'>Home</Link>
+            <Link to='/about'>About</Link>
+            <Link to='/contact'>Contact</Link>
+            <Link to='/snake'>Snake</Link>
+          </nav>
+        </header>
+        <section>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<h1>About Page</h1>} />
+            <Route path='/contact' element={<h1>Contact Page</h1>} />
+            <Route path='/snake' element={<h1>Snake Page</h1>} />
+          </Routes>
+        </section>
+      </>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,15 @@
+function Home() {
+  return (
+    <>
+      <div>
+        <h1>David Meuer</h1>
+        <p>Web Developer</p>
+      </div>
+      <div>
+        <p>Opening paragraph, Into about me</p>
+      </div>
+    </>
+  );
+}
+
+export default Home;
