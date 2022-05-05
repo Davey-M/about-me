4855e1c1f0f456e773ae84aaa3b163b8b88df65d
Davey-M <davey.meuer@gmail.com>
2022-05-05 15:11:24 -0500

    added previous background state saving and comments

--- src/Components/BackgroundCode/BackgroundCode.jsx
+++ src/Components/BackgroundCode/BackgroundCode.jsx
@@ -7,12 +7,24 @@ import { oneLight } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 import styles from './BackgroundCode.module.css';
 
+// we use this value to store the previous values of the background
+const previousBackgrounds = {};
+
 SyntaxHighlighter.registerLanguage('jsx', jsx);
 
 function BackgroundCode({ githubFilePath }) {
   const [code, setCode] = useState('');
 
+  // this useEffect hook is just for calling the api to get the file for the background
   useEffect(() => {
+    // if this file has already been used we don't call the api again
+    // this removes extra api calls and makes out page load faster after the first look
+    if (previousBackgrounds[githubFilePath]) {
+      setCode(previousBackgrounds[githubFilePath]);
+      return;
+    }
+
+    // get the file from the github api
     axios
       .get(
         'https://api.github.com/repositories/488663277/contents/' +
@@ -24,8 +36,11 @@ function BackgroundCode({ githubFilePath }) {
         },
       )
       .then((response) => {
-        // console.log(response.data);
+        // set the code to the response
         setCode(response.data);
+
+        // save the response so we don't have to call the api for this page again
+        previousBackgrounds[githubFilePath] = response.data;
       })
       .catch((err) => {
         console.error('Error in BackgroundCode', err);
